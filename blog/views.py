9f73a1e2a85b67e9da59608884375d7616cff5c2
Nazar Tsuper <nazartsuper0309@gmail.com>
2024-09-10 18:02:05 +0100

    finish view lesson

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,6 @@ from django.views import generic
 from .models import Post
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
     
