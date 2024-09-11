bae99e119ab780d08ad4b78f96e245eb0812b7c6
Nazar Tsuper <nazartsuper0309@gmail.com>
2024-09-11 18:20:31 +0100

    Add posts

--- blog/models.py
+++ blog/models.py
@@ -5,15 +5,15 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    title = models.CharField(max_length=2000, unique=True)
+    slug = models.SlugField(max_length=2000, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.CharField(max_length=200, blank=True)
+    excerpt = models.CharField(max_length=2000, blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     class Meta:
         ordering = ["-created_on"]
